8d00ba69d6b11124d3327a740916bd1d2074f941
Anton Kochkov <anton.kochkov@gmail.com>
2015-10-20 16:22:38 +0300

    Add r2pipes example to search GUIDs

---
+++ demos/Firmware/demo3_x86_uefi/search_guids.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python2
+import r2pipe
+
+# GUIDs dictionaries
+import efiguids
+
+r2 = r2pipe.open("NTFS.efi")
+for name, guid in efiguids.GUIDs.iteritems():
+    b3b2b1b0, b5b4, b7b6, b8,b9,b10,b11,b12,b13,b14,b15 = guid
+
+    b0 = b3b2b1b0 & 0xff
+    b1 = (b3b2b1b0 & 0xff00) >> 8
+    b2 = (b3b2b1b0 & 0xff0000) >> 16
+    b3 = (b3b2b1b0 & 0xff000000) >> 24
+
+    b4 = b5b4 & 0xff
+    b5 = (b5b4 & 0xff00) >> 8
+
+    b6 = b7b6 & 0xff
+    b7 = (b7b6 & 0xff00) >> 8
+
+    guid_part1 = "{0:x}{1:x}{2:x}{3:x}{4:x}{5:x}{6:x}{7:x}".format(b0,b1,b2,b3,b4,b5,b6,b7)
+    guid_part2 = "{0:x}{1:x}{2:x}{3:x}{4:x}{5:x}{6:x}{7:x}".format(b8,b9,b10,b11,b12,b13,b14,b15)
+
+    guid_search = "/xj " + guid_part1 + guid_part2
+
+    fout = open("NTFS.r2", "w+")
+    for i in r2.cmdj(guid_search):
+        print(type(i))
+        if isinstance(i,dict):
+            print("Found {0} at 0x{1:x} offset\n".format(name, i[u'offset']))
+            r2_line = "f " + name + " 16 @ " + "0x{0:x}".format(i[u'offset'])
+            print(r2_line)
+            fout.write(r2_line)
+
+
